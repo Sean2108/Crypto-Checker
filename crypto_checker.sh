144b4df2b9c0a3c05f60a5ac502309891b851fd5
Sean <sean2108@gmail.com>
2018-02-06 23:52:18 +0800

    remove unnecessary lines

--- crypto_checker.sh
+++ crypto_checker.sh
@@ -1,21 +1,4 @@
 #!/bin/bash - 
-#===============================================================================
-#
-#          FILE: eth_checker.sh
-# 
-#         USAGE: ./eth_checker.sh 
-# 
-#   DESCRIPTION: 
-# 
-#       OPTIONS: ---
-#  REQUIREMENTS: ---
-#          BUGS: ---
-#         NOTES: ---
-#        AUTHOR: YOUR NAME (), 
-#  ORGANIZATION: 
-#       CREATED: 02/06/2018 18:03
-#      REVISION:  ---
-#===============================================================================
 
 set -o nounset                              # Treat unset variables as an error
 
