2b7ac7a11436912e6ec68c933b35fb130bafcf18
Sean <sean2108@gmail.com>
2018-02-15 08:50:59 +0800

    add check for internet connection so that it terminates without a connection

--- crypto_checker.sh
+++ crypto_checker.sh
@@ -1,5 +1,8 @@
 #!/bin/bash - 
 
 set -o nounset                              # Treat unset variables as an error
+wget -q --spider http://google.com
 
-curl -s https://api.coinmarketcap.com/v1/ticker/ | python $(dirname $0)/crypto_checker.py
+if [ $? -eq 0 ]; then
+    curl -s https://api.coinmarketcap.com/v1/ticker/ | python $(dirname $0)/crypto_checker.py
+fi
