f6d955fdfb8442d537f86d43e5350a72eb48d073
Sean <sean2108@gmail.com>
2018-03-13 17:04:17 +0800

    add 1s timeout

--- crypto_checker.sh
+++ crypto_checker.sh
@@ -2,4 +2,4 @@
 
 set -o nounset                              # Treat unset variables as an error
 
-timeout 5 bash -c "curl -s https://api.coinmarketcap.com/v1/ticker/ | python $(dirname $0)/crypto_checker.py"
+timeout 1 bash -c "curl -s https://api.coinmarketcap.com/v1/ticker/ | python $(dirname $0)/crypto_checker.py"
