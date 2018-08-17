78842fb94b1250a4cc255b31d73ff5513571f07b
Sean2108 <sean2108@gmail.com>
2018-08-18 01:06:36 +0800

    improve runtime

--- crypto_checker.py
+++ crypto_checker.py
@@ -1,34 +1,35 @@
 import sys, json, requests
 from os import path
 
-def format_string(num):
-    return str(round(num, 2))
+def get_format_params(name, currency, currency_info):
+    format_string = lambda num: str(round(num, 2))
+    return [name, currency, format_string(currency_info['price']), format_string(currency_info['percent_change_1h']), currency_info['percent_change_24h']]
 
-def check_coin(coin_info):
-    if coin_info['symbol'] in tracked_coins:
-	currency_info = coin_info['quotes'][currency]
-        print('{}: {}${} (1h change: {}%, 24h change: {}%)'
-                .format(coin_info['name'], currency, format_string(currency_info['price']), format_string(currency_info['percent_change_1h']), currency_info['percent_change_24h']))
+def get_config(filename):
+    try:
+        with open(path.join(path.dirname(path.realpath(__file__)), filename), 'r') as fp:
+            config = json.load(fp)
+        tracked_coins = set(config['tracked_coins'])
+        currency = config['currency']
+        return tracked_coins, currency
+    except EnvironmentError:
+        sys.stderr.write('Config file not found\n')
+        sys.exit()
+    except KeyError:
+        sys.stderr.write('Config.json should have \"tracked_coins\" and \"currency\" attributes\n')
+        sys.exit()
 
-try:
-    with open(path.join(path.dirname(path.realpath(__file__)), 'config.json'), 'r') as fp:
-        config = json.load(fp)
-except EnvironmentError:
-    print('Config file not found')
-    sys.exit()
+def main():
+    tracked_coins, currency = get_config('config.json')
+    r = requests.get('https://api.coinmarketcap.com/v2/ticker/?convert={}'.format(currency), timeout=10)
 
-try:
-    tracked_coins = set(config['tracked_coins'])
-    currency = config['currency']
-except KeyError:
-    print('Config.json should have \"tracked_coins\" and \"currency\" attributes')
-    sys.exit()
+    if r.status_code == 200:
+        data = r.json()['data']
+        coin_dict = dict([(coin_info['symbol'], get_format_params(coin_info['name'], currency, coin_info['quotes'][currency])) for coin_info in r.json()['data'].values()])
+        for coin_symbol in tracked_coins:
+            print('{}: {}${} (1h change: {}%, 24h change: {}%)'.format(*coin_dict[coin_symbol]))
+    else:
+        print('Error code {}: Unable to get information.'.format(r.status_code))
 
-r = requests.get('https://api.coinmarketcap.com/v2/ticker/?convert={}'.format(currency), timeout=10)
-
-if r.status_code == 200:
-    data = r.json()['data']
-    for coin_index in data:
-        check_coin(data[coin_index])
-else:
-    print('Error code {}: Unable to get information.'.format(r.status_code))
+if __name__ == '__main__':
+    main()
