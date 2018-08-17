8dce99106aa9ca1819fa988bec2e93dc953996d3
Sean2108 <sean2108@gmail.com>
2018-08-18 01:58:25 +0800

    unit test

--- crypto_checker.py
+++ crypto_checker.py
@@ -3,7 +3,7 @@ from os import path
 
 def get_format_params(name, currency, currency_info):
     format_string = lambda num: str(round(num, 2))
-    return [name, currency, format_string(currency_info['price']), format_string(currency_info['percent_change_1h']), currency_info['percent_change_24h']]
+    return [name, currency, format_string(currency_info['price']), format_string(currency_info['percent_change_1h']), format_string(currency_info['percent_change_24h'])]
 
 def get_config(filename):
     try:
@@ -12,13 +12,14 @@ def get_config(filename):
         return config['tracked_coins'], config['currency']
     except EnvironmentError:
         sys.stderr.write('Config file not found\n')
-        sys.exit()
     except KeyError:
         sys.stderr.write('Config.json should have \"tracked_coins\" and \"currency\" attributes\n')
-        sys.exit()
+    return None, None
 
 def main():
     tracked_coins, currency = get_config('config.json')
+    if not tracked_coins or not currency:
+        return
     r = requests.get('https://api.coinmarketcap.com/v2/ticker/?convert={}'.format(currency), timeout=10)
 
     if r.status_code == 200:
@@ -26,7 +27,7 @@ def main():
         for coin_symbol in tracked_coins:
             print('{}: {}${} (1h change: {}%, 24h change: {}%)'.format(*coin_dict[coin_symbol]))
     else:
-        print('Error code {}: Unable to get information.'.format(r.status_code))
+        print('Error code {}: Unable to get information.'.format(str(r.status_code)))
 
 if __name__ == '__main__':
     main()
---
+++ crypto_checker_test.py
@@ -0,0 +1,79 @@
+import sys
+from crypto_checker import get_format_params, get_config
+if sys.version_info < (2, 7):
+    import unittest2 as unittest
+else:
+    import unittest
+
+class CryptoCheckerTest(unittest.TestCase):
+
+    def setUp(self):
+        self.response = {
+            "data": {
+                "1": {
+                    "id": 1, 
+                    "name": "Bitcoin", 
+                    "symbol": "BTC", 
+                    "website_slug": "bitcoin", 
+                    "rank": 1, 
+                    "circulating_supply": 17008162.0, 
+                    "total_supply": 17008162.0, 
+                    "max_supply": 21000000.0, 
+                    "quotes": {
+                        "USD": {
+                            "price": 9024.09, 
+                            "volume_24h": 8765400000.0, 
+                            "market_cap": 153483184623.0, 
+                            "percent_change_1h": -2.31, 
+                            "percent_change_24h": -4.18, 
+                            "percent_change_7d": -0.47
+                        }
+                    }, 
+                    "last_updated": 1525137271
+                }, 
+                "1027": {
+                    "id": 1027, 
+                    "name": "Ethereum", 
+                    "symbol": "ETH", 
+                    "website_slug": "ethereum", 
+                    "rank": 2, 
+                    "circulating_supply": 99151888.0, 
+                    "total_supply": 99151888.0, 
+                    "max_supply": None, 
+                    "quotes": {
+                        "USD": {
+                            "price": 642.399, 
+                            "volume_24h": 2871290000.0, 
+                            "market_cap": 63695073558.0, 
+                            "percent_change_1h": -3.75, 
+                            "percent_change_24h": -7.01, 
+                            "percent_change_7d": -2.32
+                        }
+                    }, 
+                    "last_updated": 1525137260
+                } 
+            },
+            "metadata": {
+                "timestamp": 1525137187, 
+                "num_cryptocurrencies": 1602, 
+                "error": None
+            }
+        }
+
+    def test_get_format_params_should_return_correct_format(self):
+        bitcoin_vals = self.response['data']['1']
+        result = get_format_params(bitcoin_vals['name'], 'USD', bitcoin_vals['quotes']['USD'])
+        self.assertEqual(result, ['Bitcoin', 'USD', '9024.09', '-2.31', '-4.18'])
+        
+    def test_get_config_finds_file(self):
+        coins, currency = get_config('config.json')
+        self.assertTrue(coins)
+        self.assertTrue(currency)
+
+    def test_get_config_wrong_file_raises_error(self):
+        coins, currency = get_config('does_not_exist.json')
+        self.assertFalse(coins)
+        self.assertFalse(currency)
+
+if __name__ == '__main__':
+    unittest.main()
